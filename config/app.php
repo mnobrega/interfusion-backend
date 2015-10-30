6030b4cc1fb376c35925771a9ae079fd5a47dd1d
Marcio Nobrega <marcio.nobrega@sapo.pt>
2015-10-30 18:38:42 +0000

    Added cartalyst/sentinel for user roles, permissions and management

--- config/app.php
+++ config/app.php
@@ -145,6 +145,12 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Vendor Service Providers...
+         */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Cartalyst\Sentinel\Laravel\SentinelServiceProvider::class,
+
     ],
 
     /*
@@ -193,7 +199,9 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-
+        'Activation'=> Cartalyst\Sentinel\Laravel\Facades\Activation::class,
+        'Reminder'  => Cartalyst\Sentinel\Laravel\Facades\Reminder::class,
+        'Sentinel'  => Cartalyst\Sentinel\Laravel\Facades\Sentinel::class,
     ],
 
 ];
